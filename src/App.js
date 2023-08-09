61c4d2b1fcede36117b5ce587286304301050656
Renan Mendes <renanmendes421@icloud.com>
2023-08-09 17:54:10 -0300

    Footer e Navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layouts/Container';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/' >Home</Link>
-        <Link to='/contact' >Contato</Link>
-        <Link to='/company' >Empresa</Link>
-        <Link to='/newproject' >Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass='min-height' >
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/company' element={<Company />} />
           <Route path='/newproject' element={<NewProject />} />
         </Routes>
       </Container>
-      <footer>Footer</footer>
+      
+      <Footer />
     </Router>
   );
 }
